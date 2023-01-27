8f6f77c5e7104e2952335fb54590f4abd4ce48c8
Muguai <37656342+Muguai@users.noreply.github.com>
2023-01-27 12:14:34 +0100

    Forgot to upload a couple assignments and files

---
+++ Java Assignment 2/Transaction.java
@@ -0,0 +1,33 @@
+ //Fredrik Hammar, Frha2022.
+
+package paradis.assignment2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Transaction implements Runnable {
+	private List<Operation> operations = new ArrayList<Operation>();
+	private List<Integer> accountIds = new ArrayList<Integer>();
+	private final Bank bank;
+	
+	Transaction(Bank bank) {
+		this.bank = bank;
+	}
+
+	void add(Operation operation) {
+		operations.add(operation);
+		accountIds.add(operation.getAccountId());
+	}
+	
+	List<Integer> getAccountIds() {
+		return accountIds;
+	}
+	
+	List<Operation> getOperations() {
+		return operations;
+	}
+	
+	public void run() {
+		bank.runTransaction(this);
+	}
+}	
